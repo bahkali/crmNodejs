2fc0edf7aec1447e1e19b9ebbfaa7a5854649e1e
kaly <kalimamadou@gmail.com>
2022-03-02 05:19:44 -0800

    Added the connect to mongodb using mongoose

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const routes = require("./src/routes/crmRoutes");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const app = express();
+// Mongoose connection
+const dbUri =
+  "mongodb+srv://admin:Jerico05@cluster0.fvex7.mongodb.net/CRMdb?retryWrites=true&w=majority";
+mongoose.Promise = global.Promise;
+mongoose
+  .connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => console.log("connected to db"))
+  .catch((err) => console.log(err));
 
 // MIDDLEWARE
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // ROUTES
 app.get("/", (req, res, next) => {
